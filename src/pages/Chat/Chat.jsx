7e550494d19cdc6ecc9b70d415ba6e3f575cce31
L I L V C <simidawalang@gmail.com>
2023-12-13 11:04:31 +0100

    added ui for message chats

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -24,48 +24,63 @@ const Chat = () => {
     setSendLoading(false);
   };
 
+  console.log(chatMessages);
+
   return (
     <div className={styles["chat-body"]}>
       <div className={styles["friends-list"]}>
         {friendsList.length === 0 ? (
           <p>No friends</p>
         ) : (
-          <div>
-            {friendsList.map((f, i) => (
-              <div
-                className={styles["friend-list__item"]}
-                key={i}
-                onClick={() => navigate(`/chat/${f.account}`)}
-              >
-                <FaRegCircleUser />
-                <div>
-                  <h4>{f.username}</h4>
-                  <p>{f.account}</p>
-                </div>
+          friendsList.map((f, i) => (
+            <div
+              className={styles["friend-list__item"]}
+              key={i}
+              onClick={() => navigate(`/chat/${f.account}`)}
+            >
+              <FaRegCircleUser />
+              <div>
+                <h4>{f.username}</h4>
+                <p>{f.account}</p>
               </div>
-            ))}
-          </div>
+            </div>
+          ))
         )}
       </div>
       <div className={styles["chat-user"]}>
         <h3 className={styles["current-chat__username"]}>
           @{friendsList.find((f) => f?.account === account)?.username}
         </h3>
-        <div className={styles["chat-messages"]}>
-          {chatMessages.length === 0 && (
+        <div className={styles["chat-messages__container"]}>
+          {chatMessages.length === 0 ? (
             <p>No recorded messages. Start a chat by sending a message.</p>
+          ) : (
+            chatMessages.map((c, i) => (
+              <div
+                key={i}
+                className={`${styles["chat-message"]} ${
+                  c.account !== account && styles["my-message"]
+                }`}
+              >
+                {c.msg}
+
+                <span className={styles.timestamp}>{c.time}</span>
+              </div>
+            ))
           )}
         </div>
-        <div className={styles["chat-input"]}>
-          <Input
-            className={styles.input}
-            placeholder="Type your message..."
-            value={messageToSend}
-            onChange={(e) => setMessageToSend(e.target.value)}
-          />
-          <Button onClick={handleSendMessage}>
-            {sendLoading ? <Loader /> : "Send"}
-          </Button>
+        <div className={styles["chat-input__container"]}>
+          <div className={styles["chat-input"]}>
+            <Input
+              className={styles.input}
+              placeholder="Type your message..."
+              value={messageToSend}
+              onChange={(e) => setMessageToSend(e.target.value)}
+            />
+            <Button onClick={handleSendMessage}>
+              {sendLoading ? <Loader /> : "Send"}
+            </Button>
+          </div>
         </div>
       </div>
     </div>
